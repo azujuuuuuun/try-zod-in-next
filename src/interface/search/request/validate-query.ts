a7d6df7183e3b5aadf6c27367bb03829d2073e67
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-08-06 23:38:44 +0900

    Fix validateQuery arg type

--- src/interface/search/request/validate-query.ts
+++ src/interface/search/request/validate-query.ts
@@ -1,10 +1,8 @@
-import type { GetServerSidePropsContext } from "next";
+import type { ParsedUrlQuery } from "querystring";
 import { querySchema, Query } from "./query";
 import { ValidationError } from "@/app/error/validation-error";
 
-export const validateQuery = (
-  query: GetServerSidePropsContext["query"]
-): Query => {
+export const validateQuery = (query: ParsedUrlQuery): Query => {
   try {
     return querySchema.parse(query);
   } catch (e) {
